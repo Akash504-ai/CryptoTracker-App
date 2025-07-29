515f270fb0fcb3c215cf212ae6b44501055ba807
Akash504-ai <santraakash999@gmail.com>
2025-07-29 19:52:26 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // very important for static hosting!
   plugins: [react()],
-})
+});
+
